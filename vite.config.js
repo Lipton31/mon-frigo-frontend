4b6cb5b0d73561733d0fd70d088a082390d0e3f5
Lipton <lipton31140pgmail.com>
2025-08-08 01:29:10 +0200

    Fix: Combinaison des configurations Vite pour l'API Gemini

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-});
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      external: [
+        '@google/generative-ai',
+      ]
+    },
+  },
+})
